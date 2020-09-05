297c926fcae486718db5ea06097e4807240969f1
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-09-05 23:40:12 +0200

    better

--- run.sh
+++ run.sh
@@ -4,4 +4,12 @@ dir="$(mktemp -d)"
 cd "$dir"
 curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
 cd -
-source "$dir/macports-ci" install
+arg=""
+
+test -n "$INPUT_PREFIX" && arg="--prefix=$INPUT_PREFIX"
+test -n "$INPUT_SOURCE" && arg="--source"
+source "$dir/macports-ci" install $arg
+
+if test -n "$INPUT_LOCALPORTS"; then
+    source "$dir/macports-ci" localports "$INPUT_LOCALPORTS"
+fi
