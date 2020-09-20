a3d49f910e24972f0c0136a0ec1a924976b59afd
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-09-20 02:16:34 +0200

    test

--- run.sh
+++ run.sh
@@ -4,14 +4,28 @@ dir="$(mktemp -d)"
 cd "$dir"
 curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
 cd -
+
 arg=""
+localports=""
 
-test "$1" != "" && arg="$arg --prefix=$1"
+for opt
+do
+  case "$opt" in
+  (prefix=?*)
+     arg="$arg --prefix=${opt#prefix=}"
+     ;;
+  (source=source)
+     arg="$arg --source"
+     ;;
+  (localports=?*)
+     localports="${opt#localports=}"
+     ;;
+  esac
 
-test "$2" == "source" && arg="$arg --source"
+done
 
 source "$dir/macports-ci" install $arg
 
-#if test -n "$INPUT_LOCALPORTS"; then
-    #source "$dir/macports-ci" localports "$INPUT_LOCALPORTS"
-#fi
+if test -n "$localports"; then
+    source "$dir/macports-ci" localports "$localports"
+fi
